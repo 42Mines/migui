c0300233c0cd0952c7d0d8f8045a007311ec4eb3
Adrien Bocquet <adrienbocquet38@gmail.com>
2018-01-29 00:18:43 +0100

    improved backend

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-import interface
+import interface_feu
 import cv2
 
-interface.first_fire_pass(cv2.imread("a.jpg"))
+img = cv2.imread("b.jpg")
+(x1, y1, x2, y2), _ = interface_feu.first_fire_pass(img)
+print(interface_feu.confirm_fire(img, x1, y1, x2, y2))
+
